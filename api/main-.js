6210edce33534aecaf8cb615bcabec14cd05282f
sharklatan <element.ro@gmail.com>
2018-11-24 14:22:41 -0600

    Add files via upload

---
+++ api/main-.js
@@ -0,0 +1,32 @@
+
+
+window.onload = function() {
+
+
+function appTemplate(app) {
+  return `
+    <li class="${app.TBAppName}">
+    <a href="${app.TBAppLink}~SharkApp~" class="item-link item-content external">
+    <div class="item-media"><img src="${app.TBAppIcon}" class="app-icon" width="70"></div>
+    <div class="item-inner">
+    <div class="item-title-row">
+<div class="item-title">${app.TBAppName}</div>
+<div class="item-after"><span class="badge" style="font-size: 15px; background-color: #645cff;">GET</span></div>
+</div>
+<div class="item-subtitle">${app.TBAppVersion}</div>
+<div class="item-text">${app.TBAppDescription}</div>
+</div>
+</a>
+</li>
+
+
+
+  `;
+}
+
+document.getElementById("home").innerHTML = `
+  <h1 class="app-title">APPS (${appsData.length} results)</h1>
+  ${appsData.map(appTemplate).join("")}
+  <p class="footer">Signed apps ${appsData.length}. Check back soon for updates.</p>
+`;
+};
---
+++ css/apps/viewpackage.php
@@ -0,0 +1,17 @@
+
+<html><head></head><body><div class="navbar">
+<div class="navbar-inner">
+<div class="left"><a href="#" class="back link" data-i18n="package.back"><i class="icon icon-back"></i><span>Back</span></a></div>
+<div class="center sliding" id="package-name" data-i18n="package.title">Details</div>
+<!-- <div class="right" id="package-install"></div> -->
+</div>
+</div>
+<style> .packages .item-content { height: 45px; } </style>
+<div class="pages">
+<div data-page="install-package" class="page">
+<div class="page-content">
+<div class="inner-package" style="margin-top:-1px;">
+</div>
+</div>
+</div>
+</div></body></html>
