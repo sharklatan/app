bfea6adefc4ce5add66d71e6493e0ff659cacafa
sharklatan <element.ro@gmail.com>
2020-01-30 23:25:39 -0600

    Update block.js

--- block.js
+++ block.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-if (dnsDomainIs(host, "mesu.apple.com") || (host == "appldnld.apple.com")) {
+if (dnsDomainIs(host, "mesu.apple.com") || (host == "xp.apple.com") || (host == "gdmf.apple.com") || (host == "appldnld.apple.com")) {
 return "PROXY 8.8.8.8:53";
 }
 else {
